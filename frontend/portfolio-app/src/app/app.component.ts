f0034fbcbdbe37346ba79cfd0a341c57fe39d808
emir-argcoder <ea.samsam@gmail.com>
2024-05-24 01:34:48 -0300

    Implement functional footer and nav components

--- frontend/portfolio-app/src/app/app.component.ts
+++ frontend/portfolio-app/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './shared/footer/footer.component';
+import { NavComponent } from './shared/nav/nav.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FooterComponent, NavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
